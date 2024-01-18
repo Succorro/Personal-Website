9f8c004fa3f58a8970ad3782c2e6fd94c4d2c156
Steven Gutierrez <stevogbmv@gmail.com>
2024-01-17 18:07:23 -0800

    Update nav bar styling, removed projects from home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,3 @@
-import { Card, CardHeader, Image } from '@nextui-org/react';
 
 const languages = [
   "HTML, ", "CSS, ", "JavaScript, ", "React Library, ", "Ruby, ", "Ruby on Rails, ", "Tailwind CSS"
@@ -13,11 +12,6 @@ const languageClassMap = {
   "Ruby on Rails, ": "text-red-500",
 };
 
-const projectData = [
-  { title: "Rent A Bin", imageSrc: "/code.png" },
-  // Add more projects as needed
-];
-
 const Home = () => {
   const pStyle = 'text-2xl font-bold text-gray-400 p-5';
 
@@ -25,9 +19,7 @@ const Home = () => {
     <div id='home' className="p-3 bg-background">
       <header className="m-10">
         <h1 className="text-2xl sm:text-3xl font-bold text-gray-500">
-          Hello! My name is <span className="text-cornflowerblue">Steven Gutierrez, </span> 
-          I am a <span className="text-cornflowerblue">Software Developer</span> who enjoys working on new and innovative technology. 
-          Actively seeking employment.
+          As a <span className="text-cornflowerblue">Software Developer</span> who enjoys working on new and innovative technology, I am always learning. 
         </h1>
         <p className="text-cornflowerblue m-10 font-bold text-2xl">Flatiron School Alumni</p>
         <div className="flex justify-center">
@@ -39,29 +31,6 @@ const Home = () => {
           </a>
         </div>
       </header>
-
-      <section className='not-prose'>
-        <h1 className='text-2xl font-bold'>Featured Projects</h1>
-        <div className='-mx-3 flex flex-wrap justify-center '>
-          {projectData.map((project, index) => (
-            <div key={index} className='w-full md:w-1/2 lg:w-1/3'>
-              <div className='mb-10 overflow-hidden rounded-xl bg-background dark:bg-dark-2 py-10 px-8  sm:p-12 lg:py-10 lg:px-6 xl:p-14'>
-                <Card isPressable className="col-span-12 bg-background sm:col-span-4">
-                  <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <h4 className="text-cornflowerblue font-bold text-xl">{project.title}</h4>
-                  </CardHeader>
-                  <Image
-                    removeWrapper
-                    alt="Card background"
-                    className="z-0 w-full h-full bg-background"
-                    src={project.imageSrc}
-                  />
-                </Card>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
 
       <section className="bg-gray-800 p-10 flex flex-col items-center mb-20 rounded-3xl">
         <p className={pStyle}>
