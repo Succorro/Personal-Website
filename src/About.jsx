42bb80aaa6a9726509d23bb7c356f168004ba928
Steven Gutierrez <stevogbmv@gmail.com>
2024-02-02 20:00:55 -0800

    Updated about page to have reset button on information page

--- src/About.jsx
+++ src/About.jsx
@@ -2,26 +2,47 @@ import {useState} from 'react'
 import AboutBio from "./AboutBio"
 import Education from "./Education"
 import Skills from "./Skills"
+import { useEffect } from 'react'
 
 
 function About() {
     const [imgStyle, setImgStyle] = useState('z-10 transition-all ease-in-out delay-200 m-10 opacity-100')
     const initialInformation = {
-        about: 'transition-all ease-in-out delay-200 opacity-0  fixed -my-72 sm:-my-40 mx-10',
-        skills: 'transition-all ease-in-out delay-200 opacity-0  fixed -my-72 sm:-my-40 mx-10',
-        education: 'transition-all ease-in-out delay-200 opacity-0  fixed -my-72 sm:-my-40 mx-10' 
+        about: 'transition-all ease-in-out delay-200 opacity-0  ',
+        skills: 'transition-all ease-in-out delay-200 opacity-0  ',
+        education: 'transition-all ease-in-out delay-200 opacity-0  ' 
     }
     const [information, setInformation] = useState(initialInformation)
+    const [display, setDisplay] = useState('none')
+    const [displayContent, setDisplayContent] = useState('')
 
     function handleImgClick(type){
-        setInformation({...initialInformation, [type]: 'transition-all ease-in-out delay-200 opacity-100 z-40 fixed -my-72 sm:-my-40 mx-10'})
+        setInformation({...initialInformation, [type]: 'transition-all ease-in-out delay-200 opacity-100 z-40 absolute -my-52 sm:-my-32 mx-10 '})
         setImgStyle('z-10 transition-all ease-in-out delay-200 opacity-0' )
         console.log(information)
+        setDisplay(type)
     }
-    console.log(information)
+
+    function handleBtn(){
+        setImgStyle('z-10 transition-all ease-linear delay-200 duration-0 m-10 opacity-100')
+        setInformation(initialInformation)
+        setDisplay('')
+    }
+
+    useEffect(()=>{
+         if(display === 'about'){
+        setDisplayContent(<AboutBio handleBtn={handleBtn} style={information.about}/>)
+    }else if (display === 'skills'){
+        setDisplayContent(<Skills handleBtn={handleBtn} style={information.skills}/>)
+    }else {
+        setDisplayContent(<Education handleBtn={handleBtn} style={information.education}/>)
+    }  
+    },[display])
+
+    
     return (
         <div id='about' className="p-3 text-white h-[100vh] ">
-        <div id="image flex" className="flex flex-wrap justify-center w-3/5 m-auto">
+        <div id="image flex" className="flex flex-wrap justify-center w-3/5 lg:w-full lg:mt-40 m-auto">
             <button id='btn' onClick={()=> handleImgClick('about')} className={imgStyle}>
                 <img src="/man.png" alt="avatar" />
                 <div className='z-20 -mt-16 font-bold border bg-color opacity-0 hover:opacity-100 hover:bg-color focus:bg-color transition-all ease-linear duration-0 hover:ease-out hover:duration-1000 hover:delay-100 delay-1000'>
@@ -41,10 +62,10 @@ function About() {
                 </div>
             </button>
         </div>
-
-        <AboutBio style={information.about} />
+        {displayContent}
+        {/* <AboutBio style={information.about} />
         <Skills style={information.skills}/>
-        <Education style={information.education}/>
+        <Education style={information.education}/> */}
     </div>
   )
 }
--- src/AboutBio.jsx
+++ src/AboutBio.jsx
@@ -1,21 +1,24 @@
 // eslint-disable-next-line react/prop-types
-function AboutBio({style}) {
+function AboutBio({style, handleBtn}) {
   return (
     <div className={style}>
         <div id='bio' className="bg-gray-800 rounded-lg text-start text-lg font-bold px-10 my-10 pb-5">
+            <div className="flex justify-between">
                 <h1 className="text-gray-300 underline decoration-2 decoration-background py-5">About me</h1>
-                <p className="mb-5 text-gray-300"> 
-                    Hi, I&apos;m Steven, a full stack web developer with a passion for creating clean, functional, and user-friendly web applications. 
-                    I am always looking for innovative ways to bring my clients vision to life, while adhering to best practices.
-                </p>
-                <p className="mb-5 text-gray-300">
-                    As the son of two immigrant parents I understand the value of hard work and dedication to one&apos;s craft. 
-                    I have a passion for learning about new technologies and applying my skills to my code. 
-                    My love for gaming and sports has taught me the value of teamwork, strategic thinking, and problem-solving. 
-                    My background in retail has taught me how to communicate effectively, adapt to new environments, and how to manage my time under tight deadlines. 
-                    These are skills that can tranfer to any work environment.
-                    With a strong foundation in technology and a genuine love for learning, I am committed to making a meaningful contribution to any organization that values innovation and continuous growth.
-                </p>
+                <button onClick={()=> handleBtn()} className="border my-5 px-2 rounded-3xl hover:bg-gray-500">X</button>
+            </div>
+            <p className="mb-5 text-gray-300"> 
+                Hi, I&apos;m Steven, a full stack web developer with a passion for creating clean, functional, and user-friendly web applications. 
+                I am always looking for innovative ways to bring my clients vision to life, while adhering to best practices.
+            </p>
+            <p className="mb-5 text-gray-300">
+                As the son of two immigrant parents I understand the value of hard work and dedication to one&apos;s craft. 
+                I have a passion for learning about new technologies and applying my skills to my code. 
+                My love for gaming and sports has taught me the value of teamwork, strategic thinking, and problem-solving. 
+                My background in retail has taught me how to communicate effectively, adapt to new environments, and how to manage my time under tight deadlines. 
+                These are skills that can tranfer to any work environment.
+                With a strong foundation in technology and a genuine love for learning, I am committed to making a meaningful contribution to any organization that values innovation and continuous growth.
+            </p>
         </div>
     </div>
   )
